619b684830b751da549783d93d9727777b5b5008
Rowan Gudmundsson <rgudmundsson@nevada.unr.edu>
2018-09-23 13:44:57 -0700

    Added comparison operators for VarType

--- PA05/include/BinaryTree.h
+++ PA05/include/BinaryTree.h
@@ -2,34 +2,34 @@
 
 template <typename NodeVal>
 class BinaryTree {
-  public:
-    // Constructors
-    BinaryTree();
-    BinaryTree(const BinaryTree& other);
+	public:
+		// Constructors
+		BinaryTree();
+		BinaryTree(const BinaryTree& other);
 
-    // Operators
-    BinaryTree& operator = (const BinaryTree& other);
+		// Operators
+		BinaryTree& operator = (const BinaryTree& other);
 
-    // Member functions
-    bool insert(const NodeVal& val);
-    BinaryTree* copy() const;
-    // Destructors
-    ~BinaryTree();
+		// Member functions
+		bool insert(const NodeVal& val);
+		BinaryTree* copy() const;
+		// Destructors
+		~BinaryTree();
 
-  private:
-    struct Node {
-      Node() {};
-      Node(NodeVal val, Node* left_ptr, Node* right_ptr, bool is_red):
-        data(val),
-        left(left_ptr),
-        right(right_ptr),
-        red(is_red) {}
-      NodeVal data;
-      Node* left, right;
-      bool red;
-    };
+	private:
+		struct Node {
+			Node() {};
+			Node(NodeVal val, Node* left_ptr, Node* right_ptr, bool is_red):
+				data(val),
+				left(left_ptr),
+				right(right_ptr),
+				red(is_red) {}
+			NodeVal data;
+			Node* left, right;
+			bool red;
+		};
 
-    Node* root;
+		Node* root;
 };
 
 /**
@@ -72,6 +72,7 @@ bool BinaryTree<T>::insert(const T& val) {
 
 }
 
+template<typename
 /**
  * Create a new binary tree and return a pointer to that new tree.
  * @param None
@@ -79,11 +80,12 @@ bool BinaryTree<T>::insert(const T& val) {
  */
 template<typename T>
 BinaryTree<T>* BinaryTree<T>::copy() const{
-  if (root == nullptr) {
-    return nullptr;
-  }
-  return nullptr; // For now...
+	if (root == nullptr) {
+		return nullptr;
+	}
+	return nullptr; // For now...
 }
+
 /**
  * Carefully delete all of the nodes.
  * @param None
--- PA05/include/Symbol.h
+++ PA05/include/Symbol.h
@@ -4,40 +4,47 @@
 #include <string>
 
 class Symbol {
-  public:
-    // Var type
-    struct VarType {
-      std::string name;
-      enum Type {
-        INT,
-        FLOAT,
-        CHAR
-      } type;
-    };
+	public:
+		// Var type
+		class VarType {
+			public:
+				VarType() {}
+				VarType(std::string key): name(key) {}
+				bool operator < (const VarType& rhs);
+				bool operator > (const VarType& rhs);
+				bool operator == (const VarType& rhs);
+			private:
+				std::string name;
+				enum Type {
+					INT,
+					FLOAT,
+					CHAR
+				} type;
+		};
 
-    // Constructor
-    Symbol();
-    Symbol(const Symbol& other);
+		// Constructor
+		Symbol();
+		Symbol(const Symbol& other);
 
-    // Operators
-    Symbol& operator = (const Symbol& other);
+		// Operators
+		Symbol& operator = (const Symbol& other);
 
-    // Member functions
-    bool insert(const BinaryTree<VarType>& val);
-    bool clear();
+		// Member functions
+		bool insert(const BinaryTree<VarType>& val);
+		bool clear();
 
-    BinaryTree<VarType> peek();
-    bool pop();
+		VarType* find(std::string name);
+		bool pop();
 
-    // Destructor
-    ~Symbol();
-  private:
-    struct Node {
-      Node() {}
-      Node(BinaryTree<VarType>* val, Node* next_ptr): value(val), next(next_ptr) {}
-      BinaryTree<VarType>* value;
-      Node* next;
-    };
+		// Destructor
+		~Symbol();
+	private:
+		struct Node {
+			Node() {}
+			Node(BinaryTree<VarType>* val, Node* next_ptr): value(val), next(next_ptr) {}
+			BinaryTree<VarType>* value;
+			Node* next;
+		};
 
-    Node* head;
+		Node* head;
 };
