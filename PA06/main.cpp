561c3051e6f4e5783ca45a8bf58c7bdf974c42e3
Alexander Novotny <alexander_novo@mail.tmcc.edu>
2018-10-10 11:09:12 -0700

    Separate symboTest driver

--- PA06/main.cpp
+++ PA06/main.cpp
@@ -24,42 +24,6 @@ int main(int argc, char** argv) {
 			}
 		}
 	}
-	/*Symbol table;
-	Symbol::SymbolType newVar;
-	Symbol::SymbolType* symbolPtr;
-	std::string input;
-	bool inputMode = true;
-	unsigned lineNumber = 1;
-	unsigned tabs = 0;
-	while(true) {
-		std::cout << lineNumber << " ";
-		for(unsigned i = 0; i < tabs; i++) std::cout << "\t";
-		std::cin >> input;
-		if(input == "{") {
-			table.pushScope();
-			inputMode = true;
-			tabs++;
-		} else if(input == "}") {
-			table.popScope();
-			tabs--;
-		} else if(input == ">>") {
-			inputMode = false;
-		} else if(input == "<<") {
-			inputMode = true;
-		} else if(inputMode) {
-			newVar.name = input;
-			newVar.lineNumber = lineNumber;
-			if(!table.insert(newVar)) {
-				symbolPtr = table.find(input);
-				std::cout << "Variable " << input << " was already declared on line " << symbolPtr->lineNumber << std::endl;
-			}
-		} else if((symbolPtr = table.find(input)) != nullptr) {
-			std::cout << symbolPtr->name << " declared on " << symbolPtr->lineNumber << std::endl;
-		} else {
-			std::cout << input << " not found " << std::endl;
-		}
-		lineNumber++;
-	}*/
 	return 0;
 }
 
