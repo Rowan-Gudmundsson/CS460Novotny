4a6019913aa2f91f26c33db25e0f1f7547db5987
Alexander Novotny <alexander_novo@mail.tmcc.edu>
2018-10-10 11:50:01 -0700

    Input file argument

--- main.h
+++ main.h
@@ -5,18 +5,20 @@
 #include "symbol.h"
 #include "spi-c.tab.h"
 
-// https://regex101.com/r/SizYZH/3
-//                                  v group 1     v group 2     v group 3           v group 4
-const std::regex rArgs("(?:(?:-d(?:l([1-9]?))?(?:s([1-9]?))?(?:p([1-9]?))?) ?|(?:-o ([a-zA-Z\\.-_0-9]*) ?))*");
+// https://regex101.com/r/SizYZH/4
+//                                  v group 1     v group 2     v group 3           v group 4              v group 5
+const std::regex rArgs("(?:(?:-d(?:l([1-9]?))?(?:s([1-9]?))?(?:p([1-9]?))?) ?|(?:-o ([a-zA-Z\\.-_0-9]*) ?)|([a-zA-Z\\.-_0-9]+))*");
 // Which group is which - since named groups aren't supported in c++
 #define LEX_LEVEL_GROUP 1
 #define SYM_LEVEL_GROUP 2
 #define PAR_LEVEL_GROUP 3
 #define OUT_FILE_GROUP  4
+#define IN_FILE_GROUP   5
 
 unsigned lexDLevel = 0;
 unsigned symDLevel = 0;
 unsigned parseDLevel = 0;
 std::string outputFile = "";
+std::string inputFile = "";
 
 void doCmdArgs(int argc, char** argv);
