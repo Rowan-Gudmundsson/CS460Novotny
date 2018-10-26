fe94002eedc7b682619680c57da9c05d17b27248
Alexander Novotny <alexander_novo@mail.tmcc.edu>
2018-10-25 19:04:43 -0700

    TODO: What the actual fuck?

--- node.cpp
+++ node.cpp
@@ -1,6 +1,6 @@
 #include "node.h"
 
-SyntaxNode::SyntaxNode(Type t, EvalType e, unsigned n...) : type(t), etype(e), numChildren(_numChildren), _numChildren(n) {
+SyntaxNode::SyntaxNode(Type t, EvalType e, unsigned n...) : type(t), etype(e), line(lineno), columnno(column), numChildren(_numChildren), _numChildren(n) {
 	if(numChildren > 0) {
 		va_list args;
 	    va_start(args, n);
--- node.h
+++ node.h
@@ -1,13 +1,20 @@
+#pragma once
+
 #include <cstdarg>
 #include "symbol.h"
+
+extern unsigned lineno;
+extern unsigned column;
 
 class SyntaxNode {
 	public:
 		const enum Type {
-			TYPE
+			IDENTIFIER
 		} type;
 
 		EvalType etype;
+		const unsigned line;
+		const unsigned columnno;
 
 		const unsigned& numChildren;
 		SyntaxNode** children;
@@ -19,8 +26,18 @@ class SyntaxNode {
 		unsigned _numChildren;
 };
 
-// class TypeNode : public SyntaxNode {
-// 	public:
-// 		TypeNode(EvalType eType) : SyntaxNode(TYPE, eType, 0) {}
 
-// };
+
+
+
+
+
+
+
+
+
+class IdentifierNode : public SyntaxNode {
+	public:
+		const Symbol::SymbolType* sym;
+		IdentifierNode(Symbol::SymbolType* sPtr) : SyntaxNode(IDENTIFIER, EVOID, 0), sym(sPtr) {}
+};
