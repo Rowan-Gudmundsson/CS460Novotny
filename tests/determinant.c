c614c8801d2e12708f87e7cdc4c9519a1547d103
Alexander Novotny <alexander_novo@mail.tmcc.edu>
2018-12-18 21:43:09 -0800

    Working determinant

--- tests/determinant.c
+++ tests/determinant.c
@@ -10,6 +10,7 @@ struct Matrix {
 
 int determinant(struct Matrix);
 struct Matrix readMatrix();
+void printMatrix(struct Matrix m);
 struct Matrix minor(struct Matrix, int, int);
 
 int main() {
@@ -73,35 +74,22 @@ struct Matrix readMatrix() {
 		for (j = 0; j < m.columns && j < 5; j++) { m.entries[i][j] = readInt(); }
 	}
 
+	printMatrix(m);
+
 	return m;
 }
 
 int determinant(struct Matrix m) {
 	unsigned det = 0;
 	int i, j, ticker = 1;
-	// printChar('E');
-	// printChar('N');
-	// printChar('T');
-	// printChar('E');
-	// printChar('R');
-	// printChar(':');
-	// printChar(' ');
-	// printInt((int) m.rows);
-	// printChar('X');
-	// printInt((int) m.columns);
-	// printChar((char) 10);
+
 	if (m.rows != m.columns) {
-		// printChar('E');
-		// printChar(':');
-		// printChar(' ');
-		// printInt((int) m.rows);
-		// printChar((char) 10);
 		return 0;
 	} else if (m.rows == 1)
 		return m.entries[0][0];
 
 	for (j = 0; j < m.columns; j++) {
-		det = ticker * m.entries[0][j] * determinant(minor(m, 0, j));
+		det += determinant(minor(m, 0, j)) * ticker * m.entries[0][j];
 		ticker *= 0 - 1;
 	}
 
@@ -110,8 +98,8 @@ int determinant(struct Matrix m) {
 
 struct Matrix minor(struct Matrix m, int i, int j) {
 	int a, b;
-	for (a = 0; a < m.rows - 1; a++) {
-		for (b = 0; b < m.columns - 1; b++) {
+	for (a = 0; a < m.rows; a++) {
+		for (b = 0; b < m.columns; b++) {
 			if (a >= i && b >= j) {
 				m.entries[a][b] = m.entries[a + 1][b + 1];
 			} else if (a >= i) {
@@ -122,8 +110,22 @@ struct Matrix minor(struct Matrix m, int i, int j) {
 		}
 	}
 
+	for (a = 0; a < m.rows; a++) m.entries[a][m.columns - 1] = 0;
+	for (b = 0; b < m.rows; b++) m.entries[m.rows - 1][b] = 0;
+
 	m.columns--;
 	m.rows--;
 
 	return m;
+}
+
+void printMatrix(struct Matrix m) {
+	int i, j;
+	for (i = 0; i < m.rows && i < 5; i++) {
+		for (j = 0; j < m.columns && j < 5; j++) {
+			printInt(m.entries[i][j]);
+			printChar((char) 9);
+		}
+		printChar((char) 10);
+	}
 }
